5e3db3204539efebab4dd3721435d8b8dfd0f24d
Christina Sarai Atoche Pascual <christinas.apascual@gmail.com>
2024-10-05 13:25:42 -0500

    refactor: Cambia nombre de las classes

--- js/app.js
+++ js/app.js
@@ -1,16 +1,49 @@
 // -------------------- VARIABLES --------------------
 const form = document.querySelector('#form');
+const patient = form.querySelector('#paciente');
+const owner = form.querySelector('#propietario');
+const email = form.querySelector('#email');
+const date = form.querySelector('#fecha');
+const symptoms = form.querySelector('#sintomas');
+const list = document.querySelector('#list');
+
 
 // -------------------- EVENTOS --------------------
 loadEvents()
 function loadEvents() {
-    form.addEventListener('submit', validate)
+    form.addEventListener('submit', validate);
 }
 
 // -------------------- CLASSES --------------------
+class Alert {
+
+    showAlert(type, message) {
+        const alert = document.createElement('p');
+        alert.classList.add('alert');
+        alert.textContent = message;
+
+        // agrega estilo segun el tipo
+        type === 'error' ? alert.classList.add('error') : alert.classList.add('success');
+
+        // elimina alerta existente
+        const exist = form.parentElement.querySelector('.alert');
+        exist?.remove();
+
+        // inserta alerta
+        form.parentElement.insertBefore(alert, form);
+
+        // muestra alerta durante 3 segundos
+        setTimeout(() => {
+            alert.remove();
+        }, 3000)
+    }
+}
+
+
 class Data {
+
     constructor() {
-        this.dataList = [];
+        this.listAppointments = [];
     }
 
     list(patient, owner, email, date, symptoms) {
@@ -24,71 +57,42 @@ class Data {
             id: Date.now(),
         }
 
-        this.dataList = [...this.dataList, Obj];
+        this.listAppointments = [...this.listAppointments, Obj];
+
+        this.showList();
     }
-}
+    
 
-
-class UI {
-    showAlert(type, message) {
-        const alert = document.createElement('p');
-        alert.classList.add('alert');
-        alert.textContent = message;
-
-        if(type === 'error') {
-            alert.classList.add('error');
-        } else {
-            alert.classList.add('success');
-        }
-
-        const container = document.querySelector('.container:first-child');
-
-        // elimina alerta existente
-        const exist = container.querySelector('.alert');
-        if(exist) {
-            exist.remove();
-        }
-
-        // inserta alerta
-        container.insertBefore(alert, form);
-
-        // muestra alerta durante 3 segundos
-        setTimeout(() => {
-            alert.remove();
-        }, 3000)
-    }
-
-    cleanList() {
+    showList() {
+        
         while(list.firstChild) {
             list.removeChild(list.firstChild);
         }
-    }
 
-
-    showList (dataList) {
-        const list = document.querySelector('#list');
-
-        this.cleanList();
-
-        dataList.forEach(data => {
+        this.listAppointments.forEach(data => {
             const appointment = document.createElement('div');
             appointment.classList.add('appointment');
             appointment.id = data.id;
 
             // crea html para mostar información de la cita
             const patient = document.createElement('p');
+            patient.classList.add('patient');
             patient.innerHTML = `<span class="strong">Paciente:</span> <span>${data.patient}</span>`;
             
             const owner = document.createElement('p');
+            owner.classList.add('owner');
             owner.innerHTML = `<span class="strong">Propietario:</span> <span>${data.owner}</span>`;
             
             const email = document.createElement('p');
+            email.classList.add('email');
             email.innerHTML = `<span class="strong">Email:</span> <span>${data.email}</span>`;
             
             const date = document.createElement('p');
+            date.classList.add('date')
             date.innerHTML = `<span class="strong">Fecha:</span> <span>${data.date}</span>`;
             
             const symptoms = document.createElement('p');
+            symptoms.classList.add('symptoms');
             symptoms.innerHTML = `<span class="strong">Sintomas:</span> <span>${data.symptoms}</span>`;
         
 
@@ -129,7 +133,7 @@ class UI {
 }
 
 // -------------------- INSTANCIAS --------------------
-const interface = new UI;
+const interface = new Alert;
 const information = new Data;
 
 // -------------------- FUNCIONES --------------------
@@ -137,12 +141,6 @@ const information = new Data;
 // valida formulario
 function validate(event) {
     event.preventDefault();
-
-    const patient = form.querySelector('#paciente');
-    const owner = form.querySelector('#propietario');
-    const email = form.querySelector('#email');
-    const date = form.querySelector('#fecha');
-    const symptoms = form.querySelector('#sintomas');
 
     // muestra alerta de error
     if(patient.value.trim() === '' || owner.value.trim() === '' || email.value.trim() === '' || date.value === '' || symptoms.value.trim() === '') {
@@ -163,13 +161,11 @@ function validate(event) {
     // muestra alerta de éxito
     interface.showAlert('success', 'Paciente registrado')
 
+    // envia datos del formulario
     information.list(patient.value.trim(), owner.value.trim(), email.value.trim(), date.value.trim(), symptoms.value.trim());
 
     //limpia formulario
     form.reset();
-
-    // mustra citas
-    interface.showList(information.dataList)
-
-    
 }
+
+
